8fd3dce2204d58b005d0d289b5318942815d3abd
YassineDiallo11 <e2148172@cmaisonneuve.qc.ca>
2023-04-17 13:18:53 -0400

    Ajout Bouton

--- js/carrousel.js
+++ js/carrousel.js
@@ -5,10 +5,12 @@
     let carrousel = document.querySelector('.carrousel')
     let carrousel__x = document.querySelector('.carrousel__x')
     let carrousel__figure = document.querySelector('.carrousel__figure')
- 
+    let carrousel__form = document.querySelector('.carrousel__form')
+    console.log(carrousel__form.tagName)
+
     let galerie = document.querySelector('.galerie')
     let galerie__img = galerie.querySelectorAll('img')
- 
+    console.log(galerie__img.length)
     carrousel__ouvrir.addEventListener('mousedown', function(){
        carrousel.classList.add('carrousel--activer')
        ajouter_les_images_de_galerie()
@@ -17,17 +19,44 @@
     carrousel__x.addEventListener('mousedown', function(){
        carrousel.classList.remove('carrousel--activer')
     })
- 
+
+    /**pour chaque image de la galerie ajouter dans le carrousel
+     * 
+     */
+    let position = 0
  function ajouter_les_images_de_galerie()
  {
     for (const elem of galerie__img){
-       let img = document.createElement('img')
-       img.classList.add('carrousel__img')
-       img.src = elem.src
-       // console.log(img.src)
-       carrousel__figure.appendChild(img);
+       ajouter_les_images_dans_carrousel(elem)
+       ajouter_un_radio_bouton_dans_le_carrousel()
     }
  }
  
+ /**
+  * Création dynamique d'une image pour le carrousel
+  * @param {*} elem une image de la galerie
+  */
+ function ajouter_les_images_dans_carrousel(elem)
+ {
+   let img = document.createElement('img')
+       img.classList.add('carrousel__img')
+       img.src = elem.src
+       
+       // console.log(img.src)
+       carrousel__figure.appendChild(img);
+ }
+ 
+function ajouter_un_radio_bouton_dans_le_carrousel()
+{
+   let rad = document.createElement('input')
+   rad.setAttribute('type','radio')
+   rad.setAttribute('name','carrousel__rad')
+   rad.classList.add('carrousel__rad')
+   rad.dataset.index = position 
+   position++
+   carrousel__form.append(rad)
+   
+}
+
  
  })()
