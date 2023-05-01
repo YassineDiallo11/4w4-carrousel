38913211e5abbd7621f625b703db03c1966e707d
YassineDiallo11 <e2148172@cmaisonneuve.qc.ca>
2023-05-01 13:41:37 -0400

    Ajout de la fonction pour changer l'image avec les f lèches et bouton ouvrir carrousel enlevé

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,103 +1,135 @@
-
 (function(){
-   console.log('Début du carrousel')
-   let carrousel__ouvrir = document.querySelector('.carrousel__ouvrir')
-   let carrousel = document.querySelector('.carrousel')
-   let carrousel__x = document.querySelector('.carrousel__x')
-   let carrousel__droite = document.querySelector('.carrousel__droite')
-   let carrousel__gauche = document.querySelector('.carrousel__gauche')
-   let carrousel__figure = document.querySelector('.carrousel__figure')
-   let carrousel__form = document.querySelector('.carrousel__form')
-   console.log(carrousel__form.tagName) // conteneur de radio-boutons
-
-   let galerie = document.querySelector('.galerie')
-   let galerie__img = galerie.querySelectorAll('img')
-
-   carrousel__ouvrir.addEventListener('mousedown', function(){
-      index=0
-
-      carrousel.classList.add('carrousel--activer')
-      affiche_image_carrousel()
-   })
-
-   carrousel__x.addEventListener('mousedown', function(){
-      carrousel.classList.remove('carrousel--activer')
-
-
-   })
-   carrousel__droite.addEventListener('mousedown', function(){
-
-      console.log("js Droite")
-   })
-   carrousel__gauche.addEventListener('mousedown', function(){
-      console.log("js Gauche")
-   })
+ 
+   /* -------------------------------------------------------- Variable du carrousel */
+   let carrousel  = document.querySelector(".carrousel")
+   let carrousel__x = document.querySelector(".carrousel__x")
+   let carrousel__figure = document.querySelector(".carrousel__figure")
+   let carrousel__form = document.querySelector(".carrousel__form")
+   let fleche__gauche = document.querySelector(".gauche")
+   let fleche__droite = document.querySelector(".droite")
    
-/**
- * Pour chaque image de la galerie l'ajouter dans le carrousel
- */
-let position = 0
-let index = 0
-let ancienIndex = -1
-/* -- boucle qui permet construire le carrousel */
-   for (const elem of galerie__img){
-      elem.dataset.index = position
-      /* en cliquant sur une image de la galerie */
-      elem.addEventListener('mousedown', function(e){
-         /*
-         avant d'ouvrir la boîte modale il faut vérifier si elle n'est pas déjà ouverte
-         https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
+   /* -------------------------------------------------------- Variable de la galerie */
+   let galerie = document.querySelector(".galerie")
+   let galerie__img = galerie.querySelectorAll("img")
+   // console.log("galerie__img: " + galerie__img.length)
+   // console.log(carrousel.tagName)
+   /** ---------------------------------------------------- Positionnement de l'img active du carrousel */
+   
+   let index = 0 
+   let dernier__index = -1
+   let position = 0 // permet d'indexer les images de la galerie 
 
-         la fonction contains() vous permettra de faire cette vérification
-         */
-         index = e.target.dataset.index
-         affiche_image_carrousel()
-      })
-      ajouter_une_image_dans_courrousel(elem)
-      ajouter_un_radio_bouton_dans_carrousel()
+   /** 
+    * ajouter_img_dans_carrousel
+    * Ajouter l'ensemble des images de la galerie dans la boîte modale carrousel
+    */
+   
+   for (const elm of galerie__img)
+   {
+    elm.dataset.index = position
+    elm.addEventListener('mousedown',function(){
+
+      if(carrousel.classList.contains('carrousel--activer') == false){
+        carrousel.classList.add('carrousel--activer')
+      }
+      index = this.dataset.index
+      afficher_image(index)                      
+      //console.log('numero index : ', index)
+    })
+
+    creation_img_carrousel(elm)
+    creation_radio_carrousel()
+   }
+ 
+
+   
+   
+   /* ----------------------------------------------------  fermer boîte modale */
+   carrousel__x.addEventListener('mousedown', function(){
+       //console.log('fermer la boîte modale')
+       carrousel.classList.remove('carrousel--activer')
+
+   })
+
+   /* ----------------------------------------------------  changer img avec fleches */
+
+   fleche__gauche.addEventListener('mousedown', function(){
+    //console.log('changer photo a gauche',index)
+      if(index == 0){
+        index = galerie__img.length-1
+      } else { index--}
+    afficher_image(index)
+   
+  })
+
+  fleche__droite.addEventListener('mousedown', function(){
+    //console.log('changer photo a droite',index)
+      if(index == galerie__img.length-1){
+        index=0
+      } else{index++}
+    
+    afficher_image(index)
+    
+  })
+
+ 
+   function creation_img_carrousel(elm){
+
+     //console.log(elm.getAttribute('src'))
+     let img = document.createElement('img')
+     //img.setAttribute('src', elm.getAttribute('src'))
+     img.src = elm.src
+     img.classList.add('carrousel__img')
+     //console.log (img.getAttribute('src'))
+     carrousel__figure.appendChild(img)
+     
    }
 
 
-/**
- * Création dynamique d'une image pour le carrousel
- * @param {*} elem une image de la galerie
- */
-function ajouter_une_image_dans_courrousel(elem)
-{
-   let img = document.createElement('img')
-   img.classList.add('carrousel__img')
-   img.src = elem.src
-   // console.log(img.src)
-   carrousel__figure.appendChild(img);
-}
+   /**
+    * Création d'un radio-bouton
+    */
+   
+   function creation_radio_carrousel(){
 
-function ajouter_un_radio_bouton_dans_carrousel()
-{
    let rad = document.createElement('input')
-   rad.setAttribute('type','radio')
-   rad.setAttribute('name','carrousel__rad')
+   //console.log(rad.tagName)
+   rad.setAttribute('type', 'radio')
+   rad.setAttribute('name', 'carrousel__rad')
    rad.classList.add('carrousel__rad')
-   rad.dataset.index = position
-   rad.addEventListener('mousedown', function(){
-     index =  this.dataset.index
-     affiche_image_carrousel()
-   })
-   position = position + 1 // incrémentation de la position
-   carrousel__form.append(rad)
-}  
-/**
- * Affiche la nouvelle image du carrousel
- */
-function affiche_image_carrousel(){
-   if (ancienIndex != -1){
-      carrousel__figure.children[ancienIndex].style.opacity = "0"
-    //carrousel__form.children[ancienIndex].checked = false
-      //carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer')
-     }
-     //console.log(this.dataset.index)
-     carrousel__figure.children[index].style.opacity = "1"
-    // carrousel__figure.children[index].classList.add('carrousel__img--activer')
-     ancienIndex = index
-}
+   rad.dataset.index = position // permet d'asscocier chacun des radio bouton a l'image du carrousel 
+   position++ // incrémentation de 1 
+   carrousel__form.appendChild(rad)
 
-})()
+   /** afficher image / Transition */
+
+   rad.addEventListener('mousedown', function(){
+    
+    index = this.dataset.index
+    afficher_image(index)
+    //console.log('index : ',this.dataset.index)
+    
+   })
+   }
+
+   function afficher_image(index){
+
+    if(dernier__index != -1){
+      //carrousel__figure.children[dernier__index].style.opacity = 0
+      carrousel__figure.children[dernier__index].classList.remove('carrousel__img--activer')
+      carrousel__form.children[dernier__index].checked = false 
+    } 
+    
+    carrousel__figure.children[index].classList.add('carrousel__img--activer')
+    carrousel__form.children[index].checked = true
+    dernier__index = index
+
+
+   }
+
+   /** Permet de vérifier si la classe "carrousel--activer se trrouve dans la liste des classes du carrousel"
+    * carrousel.classList.contain('carrousel--activer')
+    */
+
+
+   })()
